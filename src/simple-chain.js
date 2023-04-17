cd1d581109558ab45cd617492b0b7bbdf96935e8
Yana Mironyak <yanika.mironyak@gmail.com>
2023-04-18 02:38:01 +0300

    feat: add 2 tasks

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,32 +1,52 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement chainMaker object according to task description
- * 
+ *
  */
 const chainMaker = {
+  makers: [],
+
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.makers.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  addLink(value) {
+    if (value !== undefined) {
+      this.makers.push(`( ${value} )`);
+    } else {
+      this.makers.push(`( )`);
+    }
+
+    return chainMaker;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  removeLink(position) {
+    if (
+      typeof position !== "number" ||
+      position % 1 !== 0 ||
+      !this.makers[position - 1]
+    ) {
+      this.makers.length = 0;
+      throw new Error(`You can't remove incorrect link!`);
+    } else {
+      this.makers.splice(position - 1, 1);
+      return chainMaker;
+    }
   },
+
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.makers.reverse();
+    return chainMaker;
   },
+
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    let maker = this.makers.join(`~~`);
+    this.makers.length = 0;
+    return maker;
+  },
 };
 
 module.exports = {
-  chainMaker
+  chainMaker,
 };
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,23 +1,40 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create transformed array based on the control sequences that original
  * array contains
- * 
+ *
  * @param {Array} arr initial array
  * @returns {Array} transformed array
- * 
+ *
  * @example
- * 
+ *
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
- * 
+ *
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+
+  const arrayNew = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == "--discard-next") i++;
+    else if (arr[i] == "--discard-prev") {
+      if (arrayNew[arrayNew.length - 1] == arr[i - 1]) arrayNew.pop();
+    } else if (arr[i] == "--double-next") {
+      if (arr[i + 1]) arrayNew.push(arr[i + 1]);
+    } else if (arr[i] == "--double-prev") {
+      if (arrayNew[arrayNew.length - 1] == arr[i - 1] && arr[i - 1])
+        arrayNew.push(arr[i - 1]);
+    } else arrayNew.push(arr[i]);
+  }
+
+  return arrayNew;
 }
 
 module.exports = {
-  transform
+  transform,
 };
