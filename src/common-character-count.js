6b74a425c9e1ef3011b3e6593051da6d1852dc16
Yana Mironyak <yanika.mironyak@gmail.com>
2023-04-18 02:47:45 +0300

    feat: add 3 tasks

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given two strings, find the number of common characters between them.
@@ -11,11 +11,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function getCommonCharacterCount(s1, s2) {
+  let s11 = s1.split(""),
+    s22 = s2.split("");
+  let res = 0;
+  s11.forEach((e) => {
+    if (s22.includes(e)) {
+      res++;
+      s22.splice(s22.indexOf(e), 1);
+      console.log(s22);
+    }
+  });
+  return res;
 }
 
 module.exports = {
-  getCommonCharacterCount
+  getCommonCharacterCount,
 };
--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given an array with heights, sort them except if the value is -1.
@@ -11,11 +11,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function sortByHeight(arr) {
+  let min = [],
+    max = [],
+    res = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < 0) min.push(arr[i]);
+    else max.push(arr[i]);
+  }
+  max.sort((a, b) => b - a);
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < 0) res.push(min.pop());
+    else res.push(max.pop());
+  }
+  return res;
 }
 
 module.exports = {
-  sortByHeight
+  sortByHeight,
 };
